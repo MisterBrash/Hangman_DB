3ff7dfa683e08fe7a8d393175fb8b6df0c50ee2f
MisterBrash <matthew.brash@ocsb.ca>
2024-01-08 13:49:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 /**
- * ICS3UC Final Project (Fall 2020)
- * 
- * Student name:
- * 
- * Game: Hangman
+ * This is a sample script for getting a title from the DB
+ * Author: Mr. Brash
  */
+
+document.body.addEventListener("load", example)
+
+function example() {
+  document.getElementById("random_title").innerText = getTitle(0)
+}
 
 
