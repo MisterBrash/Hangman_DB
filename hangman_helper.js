8ff828b4af3314aaca4cf944c7583e527b01be25
MisterBrash <matthew.brash@ocsb.ca>
2024-01-08 13:25:04 -0500

    Update hangman_helper.js
    Updated the URLs

--- hangman_helper.js
+++ hangman_helper.js
@@ -1,13 +1,13 @@
-const MOVIES = "https://hangmandb.mrbrash.repl.co/Lists/AA_Movies.txt";
-const DISNEY_MOVIES = "https://hangmandb.mrbrash.repl.co/Lists/Disney_Movies.txt";
-const MLB_TEAMS = "https://hangmandb.mrbrash.repl.co/Lists/MLB_Teams.txt";
-const NHL_TEAMS = "https://hangmandb.mrbrash.repl.co/Lists/NHL_Teams.txt";
-const NBA_TEAMS = "https://hangmandb.mrbrash.repl.co/Lists/NBA_Teams.txt";
-const NFL_TEAMS = "https://hangmandb.mrbrash.repl.co/Lists/NFL_Teams.txt";
-const SPANISH = "https://hangmandb.mrbrash.repl.co/Lists/Spanish.txt";
-const WORDS = "https://hangmandb.mrbrash.repl.co/Lists/5_Letter_Words.txt";
-const BEAUTIFUL_WORDS = "https://hangmandb.mrbrash.repl.co/Lists/Beautiful_Words.txt";
-const NBA_PLAYERS = "https://hangmandb.mrbrash.repl.co/Lists/NBA_Players.txt";
+const MOVIES = "https://misterbrash.github.io/Hangman_DB/Lists/AA_Movies.txt";
+const DISNEY_MOVIES = "https://misterbrash.github.io/Hangman_DB/Lists/Disney_Movies.txt";
+const MLB_TEAMS = "https://misterbrash.github.io/Hangman_DB/Lists/MLB_Teams.txt";
+const NHL_TEAMS = "https://misterbrash.github.io/Hangman_DB/Lists/NHL_Teams.txt";
+const NBA_TEAMS = "https://misterbrash.github.io/Hangman_DB/Lists/NBA_Teams.txt";
+const NFL_TEAMS = "https://misterbrash.github.io/Hangman_DB/Lists/NFL_Teams.txt";
+const SPANISH = "https://misterbrash.github.io/Hangman_DB/Lists/Spanish.txt";
+const WORDS = "https://misterbrash.github.io/Hangman_DB/Lists/5_Letter_Words.txt";
+const BEAUTIFUL_WORDS = "https://misterbrash.github.io/Hangman_DB/Lists/Beautiful_Words.txt";
+const NBA_PLAYERS = "https://misterbrash.github.io/Hangman_DB/Lists/NBA_Players.txt";
 const lists = [MOVIES,DISNEY_MOVIES,MLB_TEAMS,NHL_TEAMS,NBA_TEAMS,NFL_TEAMS,WORDS, BEAUTIFUL_WORDS, NBA_PLAYERS, SPANISH];
 
 /** Returns an array, split on new line character by default */
